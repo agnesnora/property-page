b902496b1bdf8c3150698e629159054fd0fa09ac
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2024-04-11 15:21:34 +0200

    homeproperties

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 console.log("hello");
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
